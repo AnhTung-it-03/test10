92ff7035492aa24d7c5c7696181c7453954365ca
AnhTung-it-03 <Pyndaica000@gmail,com>
2025-06-23 08:26:03 +0700

    update

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-let weight = +prompt("nhập cân nặng ");
-let height = +prompt("nhập chiều cao ");
-let bmi = weight /(height*height);
-    if(bmi < 18.5) {
-        document.write("Underweight");
+function Bmi() {
+    let h = +document. getElementById("height").value;
+    let w = +document. getElementById("width").value;
+    h = parseInt(h);
+    w= parseInt(w)
+    let bmi = w/(h*h);
+    if (bmi < 18.5) {
+        document.getElementById("res").innerHTML = "Underweight";
+    } else if (bmi < 25.0) {
+        document.getElementById("res").innerHTML="Normal";
+    } else if (bmi < 30.0) {
+        document.getElementById("res").innerHTML="Overweight";
+    } else {
+        document.getElementById("res").innerHTML="Obese ";
     }
-    else if(18.5 < bmi < 25.0 ){
-        document.write("Normal");
-    }
-    else if(25.0 < bmi < 30.0){
-        document.write("Overweight");
-    }
-    else{
-        document.write("Normal");
-    }
-    
+}
